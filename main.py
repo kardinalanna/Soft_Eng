4ceab942e21ee55bc66296f61fc1d9e02115618d
Anna Karandina <anutajjk@yandex.ru>
2022-12-18 22:13:27 +0300

    #1 Work inline

--- main.py
+++ main.py
@@ -7,6 +7,20 @@ import hashlib
 def searcher(text):
     res = YoutubeSearch(text, max_results=10).to_dict()
     return res
-
+    
+@dp.inline_handler() #работает в inline режиме
+async def inline_handler(query: types.InlineQuery, search=""):
+    text = query.query or 'echo' #сюда попадает запрос
+    links = searcher(text) #запрос отправляем на парсинг
+        articles = [types.InlineQueryResultArticle( #окно с результатами парсинга
+        id=hashlib.md5(f'{link["id"]}'.encode()).hexdigest(),
+        title=f'{link["title"]}',
+        url=f'https://www.youtube.com/watch?v={link["id"]}',
+        thumb_url=f'{link["thumbnails"][0]}',
+        input_message_content=types.InputTextMessageContent(
+            message_text=f'https://www.youtube.com/watch?v={link["id"]}'
+        )
+    )for link in links]
+    
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
