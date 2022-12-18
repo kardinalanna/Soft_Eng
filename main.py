8bba978085581e376cde9f1aa9d4f9a3016e554e
Anna Karandina <anutajjk@yandex.ru>
2022-12-18 22:05:03 +0300

    #2#3 youtube_search lib sends a request and parses

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from config import TOKEN
 from aiogram import Bot, types, utils
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from youtube_search import YoutubeSearch
+import hashlib
+
+def searcher(text):
+    res = YoutubeSearch(text, max_results=10).to_dict()
+    return res
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
-
-executor.start_polling(dp, skip_updates=True)
